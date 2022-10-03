9cfe761872fadbfbf05a8beda80eaed25e2cfdd8
giancarlorhodes <giancarlo.rhodes@gmail.com>
2022-10-03 13:04:47 -0500

    class examples

--- 101BootcampConsoleAppHelloWorld/Program.cs
+++ 101BootcampConsoleAppHelloWorld/Program.cs
@@ -8,17 +8,41 @@ namespace _101BootcampConsoleAppHelloWorld
 {
     class Program
     {
+      
+        // Build a.net console application. Your application should
+        // build successfully and should take your name from the screen and
+        // output your name in the a Console.WriteLine() string.
+
+        // Remember Console.ReadLine() and Console.WriteLine().
+        // 1. The application should output "Hi World, "your name" programmed me!
+        // Thank you "your name!" where the "your name" is taken from the console
+        // screen and should be whatever the user types into the screen.
+        // HINT: "your name" is a C# string variable and becomes whatever
+        // the user types on the screen.
+        // Zip and upload your entire solution after it is building and working.
+        // The zipped file name should include the name of the student,
+        // the project name and date, something like "DonnyLee_HelloWorld_8122019.zip".
+
+        // Can you create a local Git Repo of this Folder? Can you create
+        // a Git Remote Repo of it?
+
         static void Main(string[] args)
         {
+            // 1. print out first line
+            Console.WriteLine("Hi World, your name programmed me!");
 
             // some code will follow this 
 
-
             // comment
-            
+
             // another comment
 
 
+            // 5. stop the window from closing
+            Console.ReadKey();
+
         }
     }
+
+
 }
